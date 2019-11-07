822ebd23a4f4fe5f55e15e423d3b28518cd7858d
Felipe Maciel <felipemrvieira@gmail.com>
2019-11-07 10:42:30 -0300

    troca recurso

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_133001) do
+ActiveRecord::Schema.define(version: 2019_11_07_134126) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
